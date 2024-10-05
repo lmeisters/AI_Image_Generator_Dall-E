43715683e350a7df611c129c4afebb1d18dac6e7
Linards <lincis20@inbox.lv>
2024-10-05 19:50:19 +0300

    Route change

--- server/index.js
+++ server/index.js
@@ -20,16 +20,16 @@ app.use(express.json({ limit: "50mb" }));
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
 
-app.get("/", async (req, res) => {
-    res.status(200).json({
-        message: "Hello from DALL-E",
-    });
-});
-
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
+app.get("/", async (req, res) => {
+    res.status(200).json({
+        message: "Hello from DALL-E",
+    });
 });
 
 const startServer = async () => {
