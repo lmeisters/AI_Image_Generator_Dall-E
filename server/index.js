058ea6800fd8bed7a873cdcad94a717f92c55aba
Linards <lincis20@inbox.lv>
2024-10-05 20:00:42 +0300

    Cloudinary security mesures

--- server/index.js
+++ server/index.js
@@ -17,6 +17,14 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
+app.use((req, res, next) => {
+    res.setHeader(
+        "Content-Security-Policy",
+        "default-src 'self'; script-src 'self' https://static.cloudflareinsights.com; style-src 'self' 'unsafe-inline' https://rsms.me;"
+    );
+    next();
+});
+
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
 
