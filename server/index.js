c0c2e2d4a36295073649c5a389d295a2f6e2d4cd
Linards <lincis20@inbox.lv>
2024-10-05 18:39:30 +0300

    Deployment test

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import connectDB from "./mongodb/connect.js";
 import postRoutes from "./routes/postRoutes.js";
@@ -8,12 +10,21 @@ import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
 app.get("/", async (req, res) => {
     res.status(200).json({
